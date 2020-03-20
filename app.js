93709c600c31c1523154344f44c4b059c414d52a
StaxGit <lario.free@gmail.com>
2020-03-20 08:23:39 +0100

    Add body-parser and setup port variable

--- app.js
+++ app.js
@@ -2,11 +2,15 @@ const express = require("express");
 const app = express();
 const mongo = require("mongodb");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const cors = require("cors");
 app.use(cors({optionSuccessStatus: 200}));
 
 app.use('/public', express.static('public')); //why this?!
+
+//body parser to be able to use req.body!
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get("/", function(req, res) {
 	res.sendFile(__dirname + "/views/index.html");
@@ -17,11 +21,12 @@ app.get("/api/hello", function(req, res) {
 });
 
 app.post("/api/shorturl/new", function(req, res) {
-	console.log(req);
+	console.log(req.body);
 	res.send("yas");
 })
 
 
-app.listen("8000", function() {
-	console.log("URL Shortener listening on " + "8000" + "!")
+let port = process.env.PORT || 8000;
+app.listen(port, function() {
+	console.log("URL Shortener listening on " + port + "!")
 })

55cd14a09a75d9559527228ef6e5990d5acf6177
StaxGit <lario.free@gmail.com>
2020-03-20 09:35:43 +0100

    Post route adds a new link to db (/w random num for now)

--- app.js
+++ app.js
@@ -14,6 +14,12 @@ app.use('/public', express.static('public')); //why this?!
 //body parser to be able to use req.body!
 app.use(bodyParser.urlencoded({extended: false}));
 
+mongoose.connect("mongodb://localhost/url_shortener",
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true 
+	});
+
 app.get("/", function(req, res) {
 	res.sendFile(__dirname + "/views/index.html");
 });
@@ -23,8 +29,20 @@ app.get("/api/hello", function(req, res) {
 });
 
 app.post("/api/shorturl/new", function(req, res) {
-	console.log(req.body);
-	res.send("yas");
+	let originalLink = req.body.url;
+	let randomNumber = Math.floor(Math.random(1)*1000); //refactor this later to count all items in db collection and set it to that number!
+	let newLink = {original_url: originalLink, short_url: randomNumber};
+	console.log(originalLink);
+	console.log(randomNumber);
+	console.log(newLink);
+	Link.create(newLink, function(err, newLink) {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log(newLink);
+			res.send("Success!!!");
+		}
+	})
 });
 
 
